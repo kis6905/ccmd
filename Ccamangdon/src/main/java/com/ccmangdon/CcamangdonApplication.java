9bcedab0d0bf26c81814a60f67f188aab11ec72a
iskwon <iskwon@iskwon-PC>
2018-05-07 18:29:50 +0900

    test

--- Ccamangdon/src/main/java/com/ccmangdon/CcamangdonApplication.java
+++ Ccamangdon/src/main/java/com/ccmangdon/CcamangdonApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CcamangdonApplication {
-	
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(CcamangdonApplication.class, args);
 	}
